8cb70a72143aa53f911d5224ac44d14692996324
OJ <ojagana@hotmail.com>
2024-08-22 10:56:44 -0400

    message

---
+++ calculate_area.js
@@ -0,0 +1,37 @@
+<html>
+<head>
+  <title>Rectangle Area Calculator</title>
+</head>
+<body>
+  <h1>Rectangle Area Calculator</h1>
+  <label for="length">Enter the length: </label>
+  <input type="number" id="length"><br><br>
+
+  <label for="width">Enter the width: </label>
+  <input type="number" id="width"><br><br>
+
+  <button onclick="calculateArea()">Calculate Area</button><br><br>
+
+  <p id="result"></p>
+<script src="./calculate_Area.js"></script>
+</body>
+</html>
+
+let length;
+let width;
+
+function calculateArea() {
+
+}
+
+function calculateArea() {
+ length = parseFloat(document.getElementById('length').value);
+ width = parseFloat(document.getElementById('width').value);
+}
+
+function calculateArea() {
+ length = parseFloat(document.getElementById('length').value);
+ width = parseFloat(document.getElementById('width').value);
+
+let area = length * width;
+}
